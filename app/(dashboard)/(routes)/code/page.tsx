52687c3584c15f62777fc5221e9d84096611c7be
vicky <vickykumarsunilja@gmail.com>
2023-12-29 15:17:08 +0530

    modified code file

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -115,7 +115,7 @@ export default function CodePage() {
           <div className="flex flex-col-reverse gap-y-2">
             {messages.map((message) => (
               <div
-                key={message.content}
+                key={message.content!}
                 className={cn(
                   "p-3 w-full flex items-start gap-x-8 rounded-lg",
                   message.role === "user"
